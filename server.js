fb83c54f035e02878042ef9c9aa6865b52dabf03
AfsanaBrishty <afsanarain603@gmail.com>
2020-08-09 15:27:44 +0600

    All html files have been transferred in public folder and changed the path in server.js necessarily

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ app.use('/static', express.static('public'))
 require('./public/model/fire_model');
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/signuppage', function(req, res) {
-  res.sendFile(path.join(__dirname, '/signuppage.html'));
+  res.sendFile(path.join(__dirname, '/public/signuppage.html'));
 });
 app.get('/Profile', function(req, res) {
-  res.sendFile(path.join(__dirname, '/Profile.html'));
+  res.sendFile(path.join(__dirname, '/public/Profile.html'));
 });
 app.get('/mapbox', function(req, res) {
-  res.sendFile(path.join(__dirname, '/mapbox.html'));
+  res.sendFile(path.join(__dirname, '/public/mapbox.html'));
 });
 app.get('/Lecture1', function(req, res) {
-  res.sendFile(path.join(__dirname, '/Lecture1.html'));
+  res.sendFile(path.join(__dirname, '/public/Lecture1.html'));
 });
 app.get('/Lecture2', function(req, res) {
-  res.sendFile(path.join(__dirname, '/Lecture2.html'));
+  res.sendFile(path.join(__dirname, '/public/Lecture2.html'));
 });
 
 var PORT= process.env.PORT || 5000;
