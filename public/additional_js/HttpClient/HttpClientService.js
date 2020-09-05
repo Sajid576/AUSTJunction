4e03b5af99448d6921594724c95215c317b880f8
Sajid576 <sajidahmed696@gmail.com>
2020-09-05 20:57:58 +0600

    multithreading issues

--- public/additional_js/HttpClient/HttpClientService.js
+++ public/additional_js/HttpClient/HttpClientService.js
@@ -163,8 +163,23 @@ window.requestStoreContactData=(uid,username,email,subject,message)=>
 //this method used to send request to fetch all the lectures according to the selected department & semester
 window.requestFetchLectures=(department,semester)=>
 {
+        var selectedDepartment = localStorage.getItem("selected_dept");
+        var selectedSemester = localStorage.getItem("selected_semester");
+        
+        console.log("department:  "+selectedDepartment)
+        console.log("semester:  "+selectedSemester)
+        //nav bar elements id
+        var dept_header=document.getElementById("dept_header");
+        var semester_header=document.getElementById("semester_header");
+        
+        dept_header.innerHTML=selectedDepartment;
+        semester_header.innerHTML=selectedSemester;
+        
+        //box elements id
+        var boxCollection=document.getElementById("boxCollection");
        
-       
+        
+        
         const url=serverUrl+'lectureApi/lectures/'+department+"/"+semester;
     
         var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
@@ -179,6 +194,49 @@ window.requestFetchLectures=(department,semester)=>
         {
             if (xmlhttp.status >= 200 || xmlhttp.status<=210) { 
               console.log(xmlhttp.response)
+
+              var lectureRef=xmlhttp.response;
+
+               var semesterRef=lectureRef[String(selectedSemester).trim()];
+              
+               if(semesterRef!=null)
+               {
+                    var output='';
+                    for (var i=0;i<semesterRef.length;i++)
+                    {
+                        var detailMap=semesterRef[i];
+                        var contributor=detailMap['contributor'];
+                        var driveLink=detailMap['drive_link'];
+                        var session=detailMap['session'];
+
+                        output+='<div  class="col-lg-4 col-md-4 col-sm-4 ">';
+                        output+= '  <div class="inner_box">';
+                        output+= '     <i class="fa fa-laptop" aria-hidden="true"></i>';
+                        output+='           <h3>Click the following button for download</h3><hr> ';
+                      
+                        output+='           <label style="color:#000000;"><u>Contributor:</u> </label><br>';       
+                        output+='           <label id="contributor-name" for="Name"style="color:#000000;font-weight:bold;">'+contributor+'</label><br>';
+                              
+                          
+                        output+='           <label style="color:#000000;"><u>Session:</u> </label> <br>';
+                         
+                        output+='           <label id="session" for="Session"style="color:#000000;font-weight:bold;">'+session+'</label><br><br>';
+                         
+                         
+                        output+='           <a href="'+driveLink+'" class="readmore">Download</a>';
+                        output+='    </div>';
+                        output+='</div>';
+                        
+                        console.log("contributor:  "+contributor+",driveLink: "+driveLink+",Session: "+session);
+                        boxCollection.innerHTML=output;
+
+                    }
+
+               }
+
+
+
+
             } else { 
               console.log(xmlhttp.response)
             }
