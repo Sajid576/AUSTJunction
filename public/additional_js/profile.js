d89e1f7a6dad20c3ee6cf9857986a33a7a170e22
AfsanaBrishty <afsanarain603@gmail.com>
2020-09-08 17:23:58 +0600

    Added nav bar in signup page

--- public/additional_js/profile.js
+++ public/additional_js/profile.js
@@ -3,26 +3,26 @@ var _editDataBtn = document.getElementById("edit_userData");
 
 window.setUserDetailsOnProfile= function()
 {
-  
+
     var _username=document.getElementById("userprofile_username");
     var _email=document.getElementById("userprofile_email");
     var _phone=document.getElementById("userprofile_phone");
     var _subscribed_bus=document.getElementById("userprofile_subscribeBus");
 
     var _subscribed_bus2= _subscribed_bus.options[_subscribed_bus.selectedIndex];
-   
+
     console.log("LEL: "+_subscribed_bus2.value);
-    _username.innerHTML=localStorage.getItem("username");
+    _username.innerHTML=localStorage.getItem("userName");
     _email.innerHTML= localStorage.getItem("email");
     _phone.innerHTML=localStorage.getItem("phone");
     _subscribed_bus2.innerHTML=localStorage.getItem("subscribed_bus");
 
-   
+
   }
 
   _editDataBtn.addEventListener('click', e => {
     e.preventDefault();
-    var uname = document.getElementById("userprofile_username");  
+    var uname = document.getElementById("userprofile_username");
     var em = document.getElementById("userprofile_email");
     var sbus = document.getElementById("userprofile_subscribeBus");
     if(uname.isContentEditable==false && em.isContentEditable==false && sbus.isContentEditable==false)
@@ -38,10 +38,10 @@ window.setUserDetailsOnProfile= function()
         uname.contentEditable=false;
         em.contentEditable=false;
         sbus.contentEditable=false;
-        var uid= localStorage.getItem("uid");
+        var uid = localStorage.getItem("uid");
         requestEditUserData(uname.value,em.value,sbus.value,uid);
 
-    }     
+    }
     if(document.getElementById("edit_userData").innerHTML="Submit")
     {
         document.getElementById("edit_userData").innerHTML="Edit";
@@ -51,6 +51,3 @@ window.setUserDetailsOnProfile= function()
         }
    }
   });
-
-   
-
