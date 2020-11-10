2cadd90edda9a96f5da188e65c37857b6a0fbe2e
Sajid576 <sajidahmed696@gmail.com>
2020-11-10 19:42:47 +0600

    module not found error solved

--- api/routes/locationtrackingApi.js
+++ api/routes/locationtrackingApi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const locationTrackingController=require('../controller/locationTrackingController');
+const locationTrackingController=require('../controller/locationtrackingController');
 
 //POST: request for storing data from tracking app
 router.post('/newLocationData',locationTrackingController.storeNewLocationData)
